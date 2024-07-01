1674ebfaf7acc3382863839cee146cb665c6cb10
Sama Ahmed <26samahmed@csu.fullerton.edu>
2024-06-30 19:35:46 -0700

    add info to landing page

---
+++ src/routes/+page.svelte
@@ -0,0 +1,31 @@
+<script>
+
+</script>
+
+<head>
+  <title>sama ahmed</title>
+  <meta name="description" content="sama ahmed's personal website">
+  <meta name="keywords" content="sama ahmed, computer science, cal state fullerton">
+  <meta name="author" content="sama ahmed">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Dosis:wght@200..800&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+
+<main>
+  <p>مرحبًا</p>
+  <p>i'm sama ahmed, a junior majoring in computer science at california state university, fullerton</p>
+</main>
+
+<style>
+  :global(body) {
+    font-family: 'Dosis', sans-serif;
+    color: #333;
+    margin: 0;
+    padding: 0;
+    font-size: 2rem;
+  }
+
+</style>
