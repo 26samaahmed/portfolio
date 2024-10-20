854f918e9197a9be2f8290eb16a998d549b6a0bb
Sama Ahmed <26samahmed@csu.fullerton.edu>
2024-10-19 20:30:13 -0700

    Add labels to projects + update resume

--- src/routes/coding-portfolio/+page.svelte
+++ src/routes/coding-portfolio/+page.svelte
@@ -10,16 +10,17 @@
   import college_highlight1 from '$lib/assets/college_highlight1.jpg';
   import college_highlight3 from '$lib/assets/college_highlight3.jpg';
   import college_highlight5 from '$lib/assets/college_highlight5.jpg';
-  import college_highlight7 from '$lib/assets/college_highlight7.jpg';
   import college_highlight9 from '$lib/assets/college_highlight9.jpg';
   import resume from '$lib/assets/Sama_Ahmed_2024_Resume.pdf';
-  
+
+  let project_type = false; // false = personal project, true = group project
 
   let projects =[
-    {name: 'SkySong', link: 'https://github.com/26samaahmed/skysong', teck_stack: 'spotify api, openweather api, python, flask, html, css, spotipy', description: 'A website where the current temperature is displayed and songs of the hour are recommended for people to listen to. The lower the temperature, the calmer the music. If the temperature is high, then upbeat music will be recommended.'},
-    {name: 'Concert Tracker', link: 'https://github.com/26samaahmed/concert-tracker', teck_stack: 'ticketmaster api, sveltekit, html, css, javascript', description: 'A website where users can search for concerts by city name and month and see the upcoming concerts in that city.'},
-    {name: 'Fullybeyond', link: 'https://github.com/26samaahmed/fullybeyond', teck_stack: 'sveltekit, tailwindCSS, javascript', description: 'A website for CSUF\'s first student-led designathon that shows information about the event like the schedule, sponsors, and judges developed with 4 other team members.'},
-    {name: 'acmcsuf.com', link: 'https://github.com/26samaahmed/acmcsuf.com', teck_stack: 'sveltekit, typescript', description: 'A website for the Association for Computing Machinery at California State University, Fullerton with 50+ contributers that shows information about the club, events, and resources. As the webmaster, I am responsible for maintaining the website and updating it with new information.'},
+    {name: 'SkySong', link: 'https://github.com/26samaahmed/skysong', teck_stack: 'python, flask, html, css, spotify api, openweather api, spotipy', description: 'A website where the current temperature is displayed and songs of the hour are recommended for people to listen to. The lower the temperature, the calmer the music. If the temperature is high, then upbeat music will be recommended.', project_type: project_type},
+    {name: 'Concert Tracker', link: 'https://github.com/26samaahmed/concert-tracker', teck_stack: 'ticketmaster api, sveltekit, html, css, javascript', description: 'A website where users can search for concerts by city name and month and see the upcoming concerts in that city.', project_type: project_type},
+    {name: 'Fullybeyond', link: 'https://github.com/26samaahmed/fullybeyond', teck_stack: 'sveltekit, tailwindCSS, javascript', description: 'A website for CSUF\'s first student-led designathon that shows information about the event like the schedule, sponsors, and judges developed with 4 other team members.', project_type: !project_type},
+    {name: 'acmcsuf.com', link: 'https://github.com/26samaahmed/acmcsuf.com', teck_stack: 'sveltekit, typescript', description: 'A website for the Association for Computing Machinery at California State University, Fullerton with 50+ contributers that shows information about the club, events, and resources. As the webmaster, I am responsible for maintaining the website and updating it with new information.',  project_type: !project_type},
+    {name: 'PlaylistExporter', link: 'https://github.com/JOwen-ster/PlaylistExporter' , teck_stack: 'python, flask, svelte, tailwindCSS, spotify api, youtube api', description: 'A website where users can export their spotify playlists to youtube playlists.', project_type: !project_type},
   ]
 
   let college_highlights = [
@@ -28,7 +29,6 @@
     {img: college_highlight3, title: 'FullyHacks Design Officer', info: '📍Hackathon with 400+ applicants, Spring 2024'},
     {img: college_highlight9, title: 'Open Source Software Co-Team Lead/Webmaster', info: '📍First Contributions Workshop, Fall 2024'},
   ]
-
 </script>
 
 <main>
@@ -53,11 +53,18 @@
   </div>
   
 
-  <div class="flex flex-col ml-5 mr-5 sm:ml-10 sm:mr-10 mt-24" id="projects">
+  <div class="flex flex-col ml-5 mr-5 sm:ml-10 sm:mr-10 mt-16" id="projects">
     <h1 class="text-4xl text-black mb-5">Projects</h1>
     {#each projects as project}
       <div class="flex flex-col bg-black text-white rounded-xl p-5 mb-7" id="project-container">
-        <h2 class="text-2xl mb-2">{project.name}</h2>
+        <div class="md:flex md:justify-between items-center">
+          <h2 class="text-2xl mb-2">{project.name}</h2>
+          {#if project.project_type == false}
+            <p class="bg-white text-sm text-black rounded-lg w-36 mb-5 text-center">Personal Project</p>
+          {:else}
+            <p class="bg-white text-sm text-black rounded-lg w-36 mb-5 text-center">Group Project</p>
+          {/if}
+        </div>
         <h3 class= "mb-2">Tech Stack: {project.teck_stack}</h3>
         <p class= "mb-2">- {project.description}</p>
         <a href={project.link} target="_blank" class="text-blue-500">View Project</a>
@@ -65,12 +72,13 @@
     {/each}
     <p class="text-lg text-black mb-5 text-end mr-4">Check out more on my <a class="underline " href="https://github.com/26samaahmed" target="_blank">github</a></p>
   </div>
+
   <div class="text-center mt-10 m-5">
     <p class="text-md mb-2">Thank you for making it this far! Here is a song recommendation for you:</p>
     <iframe 
       style="border-radius:12px; display:block; margin:auto;" 
       class="w-full sm:w-96"
-      src="https://open.spotify.com/embed/track/52dF752HWIsv2CpSuTX3p2?utm_source=generator" 
+      src="https://open.spotify.com/embed/track/3EAVnk4Dq0DPhlJLG5ljDc?utm_source=generator" 
       height="100"
       frameBorder="0" 
       allowfullscreen 
